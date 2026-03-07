7724f1a6bc064deb4519db6efc6046a715e1f568
LEGEND'S DaD <legendsdad001@gmail.com>
2026-03-07 23:46:08 +0530

    Publish

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from setuptools import setup
+from setuptools import setup, Extension
+import pybind11
 
-setup()
+ext_modules = [
+    Extension(
+        "threedimensions._core",
+        ["cpp/core.cpp"],  # change to your real C++ file path
+        include_dirs=[pybind11.get_include()],
+        language="c++",
+    ),
+]
 
+setup(
+    ext_modules=ext_modules,
+)
