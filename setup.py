2c9a19249f33caf89034371ea16826c88c4da52f
LEGEND'S DaD <legendsdad001@gmail.com>
2026-03-08 17:01:45 +0530

    Publish

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, Extension
 import pybind11
+import sys
+
+extra_compile_args = []
+
+if sys.platform == "win32":
+    extra_compile_args = ["/std:c++17"]
+else:
+    extra_compile_args = ["-std=c++17"]
 
 ext_modules = [
     Extension(
         "cpp_core",
-        ["cpp_core/bindings.cpp"],  
+        ["cpp_core/bindings.cpp"],
         include_dirs=[pybind11.get_include()],
         language="c++",
-    ),
+        extra_compile_args=extra_compile_args,
+    )
 ]
 
-setup(
-    ext_modules=ext_modules,
-)
+setup(ext_modules=ext_modules)
